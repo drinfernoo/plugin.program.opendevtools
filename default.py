80c6dc6b74de00417b4acdb20450b270fc8d42bb
drinfernoo <drinfernoo@yahoo.com>
2021-05-05 07:08:52 -0700

    :construction: - Cleanup default

--- default.py
+++ default.py
@@ -10,12 +10,12 @@ try:
 except ImportError:
     from urlparse import parse_qsl
 
+from resources.lib import color
 from resources.lib import oauth
-from resources.lib.raise_issue import raise_issue
-from resources.lib.repository import add_repository
-from resources.lib.repository import remove_repository
+from resources.lib import raise_issue
+from resources.lib import repository
 from resources.lib import settings
-from resources.lib.update_addon import update_addon
+from resources.lib import update_addon
 
 
 def _do_action():
@@ -25,20 +25,22 @@ def _do_action():
         action = params.get('action', None)
         id = params.get('id', None)
         if action == 'color_picker':
-            from resources.lib.color import color_picker
-            color_picker()
+            color.color_picker()
         elif action == 'authorize':
             oauth.authorize()
         elif action == 'revoke':
             oauth.revoke()
         elif action == 'update_addon' and id:
-            update_addon(id)
+            update_addon.update_addon(id)
     else:
         oauth.check_auth()
         dialog = xbmcgui.Dialog()
 
-        actions = [(settings.get_localized_string(32000), update_addon), (settings.get_localized_string(32001), raise_issue),
-                   (settings.get_localized_string(32002), add_repository), (settings.get_localized_string(32003), remove_repository)]
+        actions = [(settings.get_localized_string(32000), update_addon.update_addon),
+                   (settings.get_localized_string(32001), raise_issue.raise_issue),
+                   (settings.get_localized_string(32002), repository.add_repository),
+                   (settings.get_localized_string(32003), repository.remove_repository)]
+
         selection = dialog.select(settings.get_localized_string(32004), [i[0] for i in actions])
         if selection > -1:
             actions[selection][1]()
